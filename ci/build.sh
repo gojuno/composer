929d411624d675d73cd5b8c45b439584ad9debbb
Artem Zinnatullin :slowpoke <artem.zinnatullin@gmail.com>
2017-04-13 13:16:57 +0300

    Setup automatic publishing to Bintray (jcenter). (#17)

--- ci/build.sh
+++ ci/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -xe
+set -e
 
 # You can run it from any directory.
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
@@ -11,23 +11,32 @@ pushd "$PROJECT_DIR"
 USER_ID=`id -u $USER`
 
 BUILD_COMMAND="set -xe && "
-BUILD_COMMAND+="apt-get update && apt-get -y install sudo && "
-BUILD_COMMAND+="groupadd --gid $USER_ID build_user && "
-BUILD_COMMAND+="useradd --shell /bin/bash --uid $USER_ID --gid $USER_ID --create-home build_user && "
-BUILD_COMMAND+="sudo --set-home --preserve-env -u build_user "
+BUILD_COMMAND+="apt-get update && apt-get --assume-yes install git && "
+
+if [ "$USER_ID" == "0" ]; then
+    echo "Warning: running as r00t."
+else 
+    BUILD_COMMAND+="apt-get --assume-yes install sudo && "
+    BUILD_COMMAND+="groupadd --gid $USER_ID build_user && "
+    BUILD_COMMAND+="useradd --shell /bin/bash --uid $USER_ID --gid $USER_ID --create-home build_user && "
+    BUILD_COMMAND+="sudo --set-home --preserve-env -u build_user "
+fi
 
 BUILD_COMMAND+="/opt/project/gradlew "
-BUILD_COMMAND+="--no-daemon --info "
+BUILD_COMMAND+="--no-daemon --info --stacktrace "
 BUILD_COMMAND+="clean build "
 
-if [ "$PUBLISH" = "true" ]; then
-    BUILD_COMMAND+="artifactoryPublish "
+if [ "$PUBLISH" == "true" ]; then
+    BUILD_COMMAND+="bintrayUpload "
 fi
 
 BUILD_COMMAND+="--project-dir /opt/project"
 
 docker run \
--v `"pwd"`:/opt/project \
+--env BINTRAY_USER="$BINTRAY_USER" \
+--env BINTRAY_API_KEY="$BINTRAY_API_KEY" \
+--env BINTRAY_GPG_PASSPHRASE="$BINTRAY_GPG_PASSPHRASE" \
+--volume `"pwd"`:/opt/project \
 openjdk:8u121-jdk \
 bash -c "$BUILD_COMMAND"
 
