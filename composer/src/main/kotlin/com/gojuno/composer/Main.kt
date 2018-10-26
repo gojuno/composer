961add13abf7bd48686cf640f48d72fc7ecec736
Cristian Garcia <cristian.garcia@scmspain.com>
2018-10-26 11:34:01 +0200

    No need to be so explicit, type can by inferred and adding an empty collection does no bad

--- composer/src/main/kotlin/com/gojuno/composer/Main.kt
+++ composer/src/main/kotlin/com/gojuno/composer/Main.kt
@@ -111,12 +111,10 @@ private fun runAllTests(args: Args, testPackage: TestPackage.Valid, testRunner: 
                     val installTimeout = Pair(args.installTimeoutSeconds, TimeUnit.SECONDS)
                     val installAppApk = device.installApk(pathToApk = args.appApkPath, timeout = installTimeout)
                     val installTestApk = device.installApk(pathToApk = args.testApkPath, timeout = installTimeout)
-                    val installApks: MutableCollection<Observable<Unit>> = mutableListOf(installAppApk, installTestApk)
-                    if (args.extraApks.isNotEmpty()) {
-                      installApks.addAll(args.extraApks.map {
-                        device.installApk(pathToApk = it, timeout = installTimeout)
-                      })
-                    }
+                    val installApks = mutableListOf(installAppApk, installTestApk)
+                    installApks.addAll(args.extraApks.map {
+                      device.installApk(pathToApk = it, timeout = installTimeout)
+                    })
 
                     Observable
                             .concat(installApks)
